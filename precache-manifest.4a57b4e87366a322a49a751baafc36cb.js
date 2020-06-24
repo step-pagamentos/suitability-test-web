81ab9c6f87a1273c7fe55f7b89abcb8e5ec64e16
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2020-06-24 18:48:49 -0300

    Updates

---
+++ precache-manifest.4a57b4e87366a322a49a751baafc36cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff763e2c1ca9e05e8f6d1435611ce2a",
+    "url": "/suitability-test-web/index.html"
+  },
+  {
+    "revision": "cd44dfa30f13734e3eba",
+    "url": "/suitability-test-web/static/js/2.64e1be4f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/suitability-test-web/static/js/2.64e1be4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807bf0ecfbeb31d776da",
+    "url": "/suitability-test-web/static/js/main.0ccec85e.chunk.js"
+  },
+  {
+    "revision": "f65703b722e11594cdbb",
+    "url": "/suitability-test-web/static/js/runtime-main.0df32c30.js"
+  }
+]);
